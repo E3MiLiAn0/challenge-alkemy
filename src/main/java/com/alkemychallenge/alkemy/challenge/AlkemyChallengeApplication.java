a123b6369f4ef07fd2d204f6277596da214ddc43
E3MiLiAn0 <ovejero.emiliano@hotmail.es>
2022-01-21 11:50:27 -0300

    firt commit

---
+++ src/main/java/com/alkemychallenge/alkemy/challenge/AlkemyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.alkemychallenge.alkemy.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyChallengeApplication.class, args);
+	}
+
+}
