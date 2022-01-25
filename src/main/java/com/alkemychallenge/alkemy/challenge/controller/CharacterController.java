c0408733b38101228b98b3305dd75cdbda536013
E3MiLiAn0 <ovejero.emiliano@hotmail.es>
2022-01-25 18:33:15 -0300

    commit

--- src/main/java/com/alkemychallenge/alkemy/challenge/controller/CharacterController.java
+++ src/main/java/com/alkemychallenge/alkemy/challenge/controller/CharacterController.java
@@ -1,8 +1,8 @@
 package com.alkemychallenge.alkemy.challenge.controller;
 
 
+import com.alkemychallenge.alkemy.challenge.dto.CharacterDetalleDto;
 import com.alkemychallenge.alkemy.challenge.dto.CharacterDto;
-import com.alkemychallenge.alkemy.challenge.dto.CharacterDtoNameImage;
 import com.alkemychallenge.alkemy.challenge.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,33 +21,39 @@ public class CharacterController {
 
 
     @GetMapping("")
-    public ResponseEntity<List<CharacterDtoNameImage>> getAll() {
-        List<CharacterDtoNameImage> characterDtoList = this.characterService.getAll();
+    public ResponseEntity<List<CharacterDto>> getAll() {
+        List<CharacterDto> characterDtoList = this.characterService.getAll();
         return ResponseEntity.ok().body(characterDtoList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CharacterDto> getCharacter(@PathVariable(name = "id") Long id) {
-        CharacterDto characterDto = this.characterService.getOne(id);
-        return ResponseEntity.ok().body(characterDto);
+    public ResponseEntity<CharacterDetalleDto> getCharacter(@PathVariable(name = "id") Long id) {
+        CharacterDetalleDto characterDetalleDto = this.characterService.getOne(id);
+        return ResponseEntity.ok().body(characterDetalleDto);
     }
 
     @PostMapping("")
-    public ResponseEntity<CharacterDto> createCharacter(@RequestBody CharacterDto character){
-        CharacterDto characterDto = characterService.createCharacter(character);
-        return ResponseEntity.ok().body(characterDto);
+    public ResponseEntity<CharacterDetalleDto> createCharacter(@RequestBody CharacterDetalleDto character){
+        CharacterDetalleDto characterDetalleDto = characterService.createCharacter(character);
+        return ResponseEntity.ok().body(characterDetalleDto);
     }
+
 
     @PutMapping("/update/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<CharacterDto> modify(@RequestBody CharacterDto character, @PathVariable Long id) throws Exception {
-        CharacterDto characterDto = characterService.updateCharacter(character, id);
-        return ResponseEntity.ok().body(characterDto);
+    public ResponseEntity<CharacterDetalleDto> modify(@RequestBody CharacterDetalleDto character, @PathVariable Long id) throws Exception {
+
+        CharacterDetalleDto characterDetalleDto = characterService.updateCharacter(character, id);
+
+        return ResponseEntity.ok().body(characterDetalleDto);
+
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable(name = "id") long id) {
+
         this.characterService.deleteCharacter(id);
         return new ResponseEntity<>(HttpStatus.OK);
+
     }
 }
