6dc001369f5a3c117591f6579659ff288a4d60cb
LeaRamos <temple805@gmail.com>
2022-01-27 18:12:22 -0300

    algo

--- src/main/java/com/alkemychallenge/alkemy/challenge/repository/CharacterRepository.java
+++ src/main/java/com/alkemychallenge/alkemy/challenge/repository/CharacterRepository.java
@@ -1,25 +1,14 @@
 package com.alkemychallenge.alkemy.challenge.repository;
 
 import com.alkemychallenge.alkemy.challenge.model.Character;
-import com.alkemychallenge.alkemy.challenge.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface CharacterRepository extends JpaRepository<Character,Long> {
 
-     List<Character> findByNameContains( String name);
+     List<Character> findByName(String name);
 
-     List<Character> findByAge(Integer age);
-
-     @Query(
-             value = "select * from characters where characters.id in (SELECT fk_character " +
-                                                                       "FROM alkemy.rel_character_movie " +
-                                                                       "WHERE alkemy.rel_character_movie.fk_movie =:idMovie);",
-             nativeQuery = true
-     )
-     List<Character> getCharacterByIdMovieList(@Param("idMovie") Long idMovie);
+    List<Character> findByAge(Integer age);
 }
--- src/main/java/com/alkemychallenge/alkemy/challenge/service/CharacterService.java
+++ src/main/java/com/alkemychallenge/alkemy/challenge/service/CharacterService.java
@@ -13,9 +13,4 @@ public interface CharacterService {
     CharacterDetalleDto getOne(Long id);
     void deleteCharacter(Long id);
     CharacterDetalleDto updateCharacter(@Valid CharacterDetalleDto characterDetalleDto, Long id) throws Exception;
-
-    List<CharacterDetalleDto> findByName(String name);
-    List<CharacterDetalleDto> findByAge(Integer age);
-
-    List<CharacterDetalleDto> findByIdMovie(Long idMovie);
 }
